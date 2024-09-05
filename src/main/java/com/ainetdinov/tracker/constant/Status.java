c635565a65c4bc9cd74c27285a4160bbc5d104de
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-09-06 00:57:41 +0300

    Added constants

---
+++ src/main/java/com/ainetdinov/tracker/constant/Status.java
@@ -0,0 +1,5 @@
+package com.ainetdinov.tracker.constant;
+
+public enum Status {
+    OPEN, IN_PROGRESS, DONE
+}
---
+++ src/main/java/com/ainetdinov/tracker/constant/WebConstant.java
@@ -0,0 +1,18 @@
+package com.ainetdinov.tracker.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class WebConstant {
+    public static final String HIBER_CONFIG = "hibernateConfig";
+
+    // services
+    public static final String LABEL_SERVICE = "labelService";
+    public static final String HTTP_SERVICE = "httpService";
+
+    // endpoints
+    public static final String SLASH = "/";
+    public static final String ASTERISK = "*";
+    public static final String API = "/api/";
+    public static final String LABELS = "labels";
+}
