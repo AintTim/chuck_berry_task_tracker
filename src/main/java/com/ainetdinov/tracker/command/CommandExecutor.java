5a2e7a8ebc1e3e91ea138e178b1b27ead8a116aa
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-09-06 00:35:07 +0300

    CHanged to interface

--- src/main/java/com/ainetdinov/tracker/command/CommandExecutor.java
+++ src/main/java/com/ainetdinov/tracker/command/CommandExecutor.java
@@ -1,8 +1,8 @@
 package com.ainetdinov.tracker.command;
 
-public class CommandExecutor {
+public interface CommandExecutor {
 
-    public <T> T executeCommand(Command<T> command) {
+    default  <T> T executeCommand(Command<T> command) {
         return command.execute();
     }
 }
