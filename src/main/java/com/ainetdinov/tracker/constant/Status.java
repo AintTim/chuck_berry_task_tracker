394d330491ad7638379f874a7285c336b3926b31
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-09-16 10:21:42 +0300

    status enum added

--- src/main/java/com/ainetdinov/tracker/constant/Status.java
+++ src/main/java/com/ainetdinov/tracker/constant/Status.java
@@ -1,5 +1,25 @@
 package com.ainetdinov.tracker.constant;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
 public enum Status {
-    OPEN, IN_PROGRESS, DONE
+    OPEN("open"),
+    IN_PROGRESS("in-progress"),
+    DONE("done");
+
+    private final String text;
+
+    Status(String text) {
+        this.text = text;
+    }
+
+    public static Status getStatus(String text) {
+        return Arrays.stream(Status.values())
+                .filter(s-> s.text.equals(text))
+                .findFirst()
+                .orElse(null);
+    }
 }
